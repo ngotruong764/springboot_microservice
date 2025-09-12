4ccfdf6a2f9848017bc368a8e1dc6b123fc057d0
ngotruong764 <truongnq.ba12-180@st.usth.edu.vn>
2025-09-12 09:04:18 +0700

    update sec15 - K8s

---
+++ section14/accounts/src/main/java/com/eazybytes/accounts/service/ICustomerService.java
@@ -0,0 +1,12 @@
+package com.eazybytes.accounts.service;
+
+import com.eazybytes.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomerService {
+    /**
+     *
+     * @param mobileNumber - Input mobile number
+     * @return Customer Details based on a given mobile number
+     */
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber, String correlationId);
+}
